5e4d6dfca898c6cc1ed1528e340eab56c51992ff
Anatoly SIdash <sidash85@yandex.ru>
2023-07-31 20:57:40 +0300

    fix: fix review mistakes

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
-const BadRequestError = require('../errors/bad-request-error');
+const NotAuthorizedRequestError = require('../errors/not-authorized-request-error');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -45,13 +45,13 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        throw new BadRequestError('Неправильные почта или пароль');
+        throw new NotAuthorizedRequestError('Неправильные почта или пароль');
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            throw new BadRequestError('Неправильные почта или пароль');
+            throw new NotAuthorizedRequestError('Неправильные почта или пароль');
           }
           return user;
         });
